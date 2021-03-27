0b87733d887431c6814c3c8350e3c34a543c3692
Meng-Chieh Lin <anton850117food1@gmail.com>
2021-03-27 15:18:19 +0800

    first commit

---
+++ C++/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Rika/Documents/GitHub/C++/Cmake Test
+# Build directory: C:/Users/Rika/Documents/GitHub/C++/Cmake Test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ java/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(System.getProperty("java.vendor"));
+        System.out.println(System.getProperty("java.vendor.url"));
+        System.out.println(System.getProperty("java.version"));
+    }
+}
