1af1404b183c4cbd88b9b7e51c138cdd9f2212d8
Jay Lin <anton850117ma@gmail.com>
2022-05-02 15:35:16 +0800

    update: new test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <windows.h>
 
-int main()
-{
-    cout << "gg";
-    return 0;
-}
+using namespace System;
+
+namespace test {
+   public
+    ref class Arith {
+       public:
+        /// <summary>
+        /// add two numbers
+        /// </summary>
+        static int Add(int a, int b) { return a + b; }
+        static int Sub(int a, int b) { return a - b; }
+        static int Mul(int a, int b) { return a * b; }
+        static int Div(int a, int b) { return a / b; }
+    };
+}    // namespace test
