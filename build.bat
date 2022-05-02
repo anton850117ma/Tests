3475a52573e62e3c39c38d5b43fbfbc0608bd9ca
Jay Lin <anton850117ma@gmail.com>
2022-05-02 15:47:40 +0800

    update: files

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set DIR=%cd%
-cmake CMakeLists.txt -G "Visual Studio 17 2022" -A x64
+
 cmake -S %DIR% -B %DIR%\build
-@REM cmake --build %DIR%\build --config Release
+cmake --build %DIR%\build --config Release
